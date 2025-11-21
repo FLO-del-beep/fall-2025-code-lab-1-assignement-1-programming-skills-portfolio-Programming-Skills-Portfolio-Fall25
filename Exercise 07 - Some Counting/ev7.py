22c8030f575c7f0f15a7b9cbb39b4c760e7c6024
FLO-del-beep <nheilzymonilustrisimo@gmail.com>
2025-11-21 20:37:06 +0400

    counting code

---
+++ Exercise 07 - Some Counting/ev7.py
@@ -0,0 +1,19 @@
+# Count up from 0 to 50 in steps of 1
+for i in range(0, 51, 1):   # 51 because stop is exclusive
+    print(i)
+
+# Count down from 50 to 0 in steps of -1
+for i in range(50, -1, -1): # -1 because stop is exclusive
+    print(i)
+
+# Count up from 30 to 50 in steps of 1
+for i in range(30, 51, 1):
+    print(i)
+
+# Count down from 50 to 10 in steps of -2
+for i in range(50, 9, -2):
+    print(i)
+
+# Count up from 100 to 200 in steps of 5
+for i in range(100, 201, 5):
+    print(i)
