ee0d78d6913b7df2066932f53ad57468ba7d8078
FLO-del-beep <nheilzymonilustrisimo@gmail.com>
2025-11-21 20:36:25 +0400

    bio code

---
+++ Exercise 03 - Biography/ex3.py
@@ -0,0 +1,14 @@
+Des={
+    'name':'Name',
+    'hometown':'Hometown',
+    'age':'Age',
+    'greetings':'Hello'
+} # dictionary to keep important things
+
+print(Des['greetings']) #greeting message
+
+User_name= input("Enter your full name:") #ask for users input
+User_age= input("Whats your age?")
+User_hometown= input("Where are you from?")
+print("Thank you for your answer")
+print=input()
